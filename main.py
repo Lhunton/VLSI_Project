550cedb2b58012cb92291a98edf82576ce582f16
Lhunton <lewishunton0@gmail.com>
2025-10-08 13:32:49 +0100

    Start of data processing

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
 import math
 import random
 
@@ -18,13 +19,13 @@ torch.manual_seed(6)
 #Constant definitions
 inputNodes = 64
 hiddenNodes = 64
-outputNodes = 16
+outputNodes = 10
 
 learningRate = 0.1
 alpha = 1
 
-hiddenNodesWeights = [[0]*inputNodes]*hiddenNodes
-outputNodesWeights = [[0]*hiddenNodes]*outputNodes
+hiddenNodesWeights = [[random.random() for _ in range(inputNodes)] for _ in range(hiddenNodes)]
+outputNodesWeights = [[random.random() for _ in range(hiddenNodes)] for _ in range(outputNodes)]
 
 outputNodesBias = [0]*outputNodes
 
@@ -34,18 +35,24 @@ digits = load_digits()
 def ELU(x):
     return x if x > 0 else alpha*math.exp(x) - 1
 
+def normalise(x):
+    return x/16.0
+
 def startTraining():
-    for x in range (0,inputNodes):
-        for y in range (0,hiddenNodes):
-            hiddenNodesWeights[x][y] = random.random()
+    hiddenNodesWeights = torch.rand(inputNodes,hiddenNodes)
+    outputNodesWeights = torch.rand(hiddenNodes,outputNodes)
 
-    for n in range (0,hiddenNodes):
-        for o in range (0,outputNodes):
-            outputNodesWeights[o][n] = random.random()
+#Data processing definitions
+def dataPreprocessing(x,y):
+    normalisedX = normalise(x)
 
+    tempTensorx = torch.FloatTensor(normalisedX)
+    tempTensory = torch.LongTensor(y)
+
+    return tempTensorx, tempTensory
+
+#Main
 def main():
-    """Main function"""
-
     startTraining()
 
     print("Available keys:", digits.keys())
@@ -55,14 +62,31 @@ def main():
     print("Target names:", digits.target_names)
 
     # Access the data and labels
-    X = digits.data
+    x = digits.data
     y = digits.target
+
+    #Function for splitting data set up and holding some data points in reserve for data validation  and credibility during report writing
+    X_train, X_test, y_train, y_test = train_test_split(
+    x, y, 
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+    )
+
+
+
+
+
+
+
+
 
     # Display the first image
     plt.figure(figsize=(8, 6))
     plt.imshow(digits.images[0], cmap='gray')
     plt.title(f"Digit: {digits.target[0]}")
     plt.colorbar()
+    print(digits.images[0])
     plt.show()
 
 if __name__ == "__main__":
